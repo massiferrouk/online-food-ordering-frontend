0b40cd17d5262c784c957d8c031258b260ff50a8
massiferrouk <massiferrouk3@gmail.com>
2025-03-22 13:06:43 +0100

    home page finie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar/Navbar';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './Theme/DarkTheme';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        <Navbar/>
+        <Home/>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/Home/MultiItemCarousel.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import { topMeels } from './TopMeel';
+import CarouselItem from './CarouselItem';
+import { Slideshow } from '@mui/icons-material';
+
+const MultiItemCarousel = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true
+    }
+
+  return (
+    <div>
+        <Slider {...settings}>
+            {topMeels.map((item) => <CarouselItem image={item.image} title={item.title} />)}
+        </Slider>
+    </div>
+  )
+}
+
+
+export default MultiItemCarousel;
