35ba57c51da3dc8558d3321378a46ba16ce02689
massiferrouk <massiferrouk3@gmail.com>
2025-04-24 00:14:04 +0200

    connexion de Order

---
+++ src/components/State/Order/Reducer.js
@@ -0,0 +1,34 @@
+import { GET_USER_FAILURE } from "../Authentication/ActionType";
+import { GET_USERS_NOTIFICATION_SUCCESS, GET_USERS_ORDERS_FAILURE, GET_USERS_ORDERS_REQUEST, GET_USERS_ORDERS_SUCCESS } from "./ActionType";
+
+const initialState = {
+    loading: false,
+    error: null,
+    orders: [],
+}
+
+export const orderReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case GET_USERS_ORDERS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case GET_USERS_ORDERS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                orders: payload,
+                error: null
+            };
+        case GET_USERS_ORDERS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: payload,
+            };
+        default:
+            return state;
+    }
+}
