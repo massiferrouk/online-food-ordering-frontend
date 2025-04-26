715845f271cf03c177553da5a81f877f72ff922b
massiferrouk <massiferrouk3@gmail.com>
2025-04-26 21:13:33 +0200

    Action de Ingredients

--- src/components/State/Ingredients/Action.js
+++ src/components/State/Ingredients/Action.js
@@ -51,3 +51,68 @@ export const createIngredient = ({ data, jwt }) => {
         }
     }
 }
+
+export const createIngredientCategory = ({ data, jwt }) => {
+    console.log("data ", data, "jwt", jwt)
+    return async (dispatch) => {
+        try {
+            const response = await api.post(`/api/admin/ingredients/category`, data, {
+                headers: {
+                    Authorization: `Bearer ${jwt}`,
+                },
+            })
+            console.log("create ingredients category", response.data)
+            dispatch({
+                type: CREATE_INGREDIENT_CATEGORY_SUCCESS,
+                payload: response.data,
+            })
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+}
+
+export const getIngredientCategory = ({ id, jwt }) => {
+    return async (dispatch) => {
+        try {
+            const response = await api.get(
+                `/api/admin/ingredients/restaurant/${id}/category`, 
+                {
+                    headers: {
+                        Authorization: `Bearer ${jwt}`,
+                    }
+                })
+            console.log("get ingredients category", response.data)
+            dispatch({
+                type: GET_INGREDIENT_CATEGORY_SUCCESS,
+                payload: response.data,
+
+            })
+        } catch (error) {
+            console.log("error ", error)
+        }
+    }
+}
+
+export const updateStockOfIngredient = ({ id, jwt }) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await api.put(
+                `/api/admin/ingredients/${id}/stoke`,
+                {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${jwt}`,
+                    },
+                }
+            )
+            dispatch({
+                type: UPDATE_STOCK,
+                payload: data,
+            })
+            console.log("update ingredients stock ", data)
+        } catch (error) {
+            console.log("error ", error)
+        }
+    }
+}
